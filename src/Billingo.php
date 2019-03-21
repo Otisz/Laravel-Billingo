d8913a11421e2b6d4b8739dc28298dae36ba6812
Levente Otta <leventeotta@gmail.com>
2019-03-21 22:45:42 +0100

    Move methods from BillinGoblin to Billingo, and implement contract

--- src/Billingo.php
+++ src/Billingo.php
@@ -8,7 +8,8 @@
 
 namespace Otisz\Billingo;
 
-use Otisz\Billingo\Abstracts\BillinGoblin;
+use Billingo\API\Connector\HTTP\Request as BillingoConnector;
+use Otisz\Billingo\Contracts\Billingo as BillingoContract;
 
 /**
  * Class Billingo
@@ -17,7 +18,60 @@ use Otisz\Billingo\Abstracts\BillinGoblin;
  *
  * @package Otisz\Billingo
  */
-class Billingo extends BillinGoblin
+class Billingo implements BillingoContract
 {
-    //
+    /**
+     * @var \Billingo\API\Connector\HTTP\Request
+     */
+    private static $connector;
+
+    /**
+     * BillinGoblin constructor.
+     *
+     * @param \Billingo\API\Connector\HTTP\Request $connector
+     */
+    public function __construct(BillingoConnector $connector)
+    {
+        self::$connector = $connector;
+    }
+
+    /**
+     * @author Levente Otta <leventeotta@gmail.com>
+     *
+     * @param string $uri
+     * @param array $payload
+     *
+     * @return mixed|\Psr\Http\Message\ResponseInterface
+     * @throws \Billingo\API\Connector\Exceptions\JSONParseException
+     * @throws \Billingo\API\Connector\Exceptions\RequestErrorException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public static function get(string $uri, array $payload = [])
+    {
+        return self::$connector->get($uri, $payload);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function post(string $uri, array $payload = [])
+    {
+        return self::$connector->post($uri, $payload);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function put(string $uri, array $payload = [])
+    {
+        return self::$connector->put($uri, $payload);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function delete(string $uri, array $payload = [])
+    {
+        return self::$connector->delete($uri, $payload);
+    }
 }
