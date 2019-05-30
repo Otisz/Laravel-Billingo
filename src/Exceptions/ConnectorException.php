24e196e5ec910a7497289c8bbfd77b3349d86711
Levente Otta <leventeotta@gmail.com>
2019-05-30 22:59:01 +0200

    Implement connector into package

---
+++ src/Exceptions/ConnectorException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Deployed by Levente Otta <leventeotta@gmail.com>
+ *
+ * @author Levente Otta <leventeotta@gmail.com>
+ * @copyright Copyright (c) 2019. Levente Otta
+ */
+
+namespace Otisz\Billingo\Exceptions;
+
+use Exception;
+
+/**
+ * Class ConnectorException
+ *
+ * @package Otisz\Billingo\Exceptions
+ */
+class ConnectorException extends Exception
+{
+    //
+}
