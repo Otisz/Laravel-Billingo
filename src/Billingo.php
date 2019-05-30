43bc81c319b7553240f344cd67e6907e37752cf5
Levente Otta <leventeotta@gmail.com>
2019-05-30 23:01:31 +0200

    Add services to facade

--- src/Billingo.php
+++ src/Billingo.php
@@ -8,70 +8,149 @@
 
 namespace Otisz\Billingo;
 
-use Otisz\BillingoConnector\Connector;
-use Otisz\Billingo\Contracts\Billingo as BillingoContract;
+use Otisz\Billingo\Connector\Connector;
+use Otisz\Billingo\Contracts\Billable;
+use Otisz\Billingo\Services\BankAccounts;
+use Otisz\Billingo\Services\Clients;
+use Otisz\Billingo\Services\Currency;
+use Otisz\Billingo\Services\Expenses;
+use Otisz\Billingo\Services\Invoices;
+use Otisz\Billingo\Services\PaymentMethods;
+use Otisz\Billingo\Services\Products;
+use Otisz\Billingo\Services\Recurring;
+use Otisz\Billingo\Services\Vat;
 
 /**
  * Class Billingo
  *
- * @author Levente Otta <leventeotta@gmail.com>
- *
  * @package Otisz\Billingo
  */
-class Billingo implements BillingoContract
+class Billingo implements Billable
 {
     /**
-     * @var \Otisz\BillingoConnector\Connector $connector
+     * @var \Otisz\Billingo\Connector\Connector $connector
      */
-    private static $connector;
+    private $connector;
 
     /**
      * Billingo constructor.
      *
-     * @param \Otisz\BillingoConnector\Connector $connector
+     * @param \Otisz\Billingo\Connector\Connector $connector
      */
     public function __construct(Connector $connector)
     {
-        self::$connector = $connector;
+        $this->connector = $connector;
     }
 
     /**
      * @inheritDoc
      */
-    public static function connector(): Connector
+    public function connector(): Connector
     {
-        return self::$connector;
+        return $this->connector;
     }
 
     /**
      * @inheritDoc
      */
-    public static function get(string $uri, array $payload = [])
+    public function get(string $uri, array $payload = [])
     {
-        return self::$connector->get($uri, $payload);
+        return $this->connector->get($uri, $payload);
     }
 
     /**
      * @inheritdoc
      */
-    public static function post(string $uri, array $payload = [])
+    public function post(string $uri, array $payload = [])
     {
-        return self::$connector->post($uri, $payload);
+        return $this->connector->post($uri, $payload);
     }
 
     /**
      * @inheritdoc
      */
-    public static function put(string $uri, array $payload = [])
+    public function put(string $uri, array $payload = [])
     {
-        return self::$connector->put($uri, $payload);
+        return $this->connector->put($uri, $payload);
     }
 
     /**
      * @inheritdoc
      */
-    public static function delete(string $uri, array $payload = [])
+    public function delete(string $uri, array $payload = [])
     {
-        return self::$connector->delete($uri, $payload);
+        return $this->connector->delete($uri, $payload);
+    }
+
+    /**
+     * @return \Otisz\Billingo\Services\Clients
+     */
+    public function clients(): Clients
+    {
+        return new Clients;
+    }
+
+    /**
+     * @return \Otisz\Billingo\Services\Invoices
+     */
+    public function invoices(): Invoices
+    {
+        return new Invoices;
+    }
+
+    /**
+     * @return \Otisz\Billingo\Services\Products
+     */
+    public function products(): Products
+    {
+        return new Products;
+    }
+
+    /**
+     * @return \Otisz\Billingo\Services\BankAccounts
+     */
+    public function bankAccounts(): BankAccounts
+    {
+        return new BankAccounts;
+    }
+
+    /**
+     * @return \Otisz\Billingo\Services\Currency
+     */
+    public function currency(): Currency
+    {
+        return new Currency;
+    }
+
+    /**
+     * @return \Otisz\Billingo\Services\Expenses
+     */
+    public function expenses(): Expenses
+    {
+        return new Expenses;
+    }
+
+    /**
+     * @return \Otisz\Billingo\Services\PaymentMethods
+     */
+    public function paymentMethods(): PaymentMethods
+    {
+        return new PaymentMethods;
+    }
+
+    /**
+     * @return \Otisz\Billingo\Services\Recurring
+     */
+    public function recurring(): Recurring
+    {
+        return new Recurring;
+    }
+
+    /**
+     * @return \Otisz\Billingo\Services\Vat
+     */
+    public function vat(): Vat
+    {
+        return new Vat;
     }
 }
--- src/Facades/Billingo.php
+++ src/Facades/Billingo.php
@@ -13,9 +13,22 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Class Billingo
  *
- * @author Levente Otta <leventeotta@gmail.com>
- *
  * @package Otisz\Billingo\Facades
+ *
+ * @method static mixed|\Psr\Http\Message\ResponseInterface get(string $uri, array $payload = [])
+ * @method static mixed|\Psr\Http\Message\ResponseInterface post(string $uri, array $payload = [])
+ * @method static mixed|\Psr\Http\Message\ResponseInterface put(string $uri, array $payload = [])
+ * @method static mixed|\Psr\Http\Message\ResponseInterface delete(string $uri, array $payload = [])
+ * @method static \Otisz\Billingo\Connector\Connector connector()
+ * @method static \Otisz\Billingo\Services\Clients clients()
+ * @method static \Otisz\Billingo\Services\Invoices invoices()
+ * @method static \Otisz\Billingo\Services\Products products()
+ * @method static \Otisz\Billingo\Services\BankAccounts bankAccounts()
+ * @method static \Otisz\Billingo\Services\Currency currency()
+ * @method static \Otisz\Billingo\Services\Expenses expenses()
+ * @method static \Otisz\Billingo\Services\PaymentMethods paymentMethods()
+ * @method static \Otisz\Billingo\Services\Recurring recurring()
+ * @method static \Otisz\Billingo\Services\Vat vat()
  */
 class Billingo extends Facade
 {
